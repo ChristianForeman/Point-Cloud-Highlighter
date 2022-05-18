eea47188a4e8beb2a9bca268f2ed4daed919cd7a
ChristianForeman <cjforema@umich.edu>
2022-05-18 16:04:57 -0400

    cleaned up code, added comments, a few more todos before fully clean

--- src/pc_selector.py
+++ src/pc_selector.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+# FIXME: Simplify imports, dont do import *, simplify utils.py
 import argparse
 import pathlib
 
@@ -13,18 +14,22 @@ import numpy as np
 import rospy
 import sensor_msgs.msg
 import sensor_msgs.point_cloud2
-from arc_utilities.ros_helpers import get_connected_publisher
 
 
 class TrajectoryPlaybackGUI(QWidget):
     def __init__(self, frame, default_radius, filename: pathlib.Path):
         super().__init__()
-
         # Read in arguments
         self.frame_id = frame
         self.radius = default_radius
         self.bag_path = filename
 
+        # Initialize publishers
+        self.sphere_pub = rospy.Publisher("/sel_data/sel_sphere", Marker, queue_size=10)
+        self.frame_pub = rospy.Publisher("/sel_data/cur_frame", PointCloud2, queue_size=10)
+        self.sel_pub = rospy.Publisher("/sel_data/selected_pc", PointCloud2, queue_size=10)
+
+        # TODO: Check if there are faster ways to read in a bag
         # Read in the bag
         bag = rosbag.Bag(filename.as_posix())
         self.bag_msgs = []
@@ -38,36 +43,57 @@ class TrajectoryPlaybackGUI(QWidget):
         self.points = np.array(list(sensor_msgs.point_cloud2.read_points(self.bag_msgs[0])))
 
         # Set up the sphere marker
-        self.sphere_pub = rospy.Publisher("/sel_data/sel_sphere", Marker, queue_size=10)
         self.sphere_marker = Marker()
         self.setup_sphere_marker()
 
         # Set up the interactive marker
         self.setup_interactive_marker()
 
-        # Set up the publisher for selected points
-        self.sel_pub = rospy.Publisher("/selected_pc", PointCloud2, queue_size=10)
-
-        # Frame_counter is used to skip every other message
-        self.frame_counter = 0
+        # frame_idx in the index of the bag we are currently viewing
+        self.frame_idx = 0
+        # Publish the 0th frame of the bag to the pointcloud
+        self.frame_pub.publish(self.bag_msgs[0])
 
         # Set up the UI
         self.layout = QVBoxLayout(self)
-        self.setup_UI()
 
-        self.frame_pub = rospy.Publisher("/sel_data/cur_frame", PointCloud2, queue_size=10)
+        # Radius Label
+        self.label = QLabel('0', self)
+        self.label.setAlignment(Qt.AlignCenter | Qt.AlignVCenter)
+        self.label.setText("Radius Selector")
+        self.layout.addSpacing(15)
+        self.layout.addWidget(self.label)
 
-        self.frame_idx = 0
-        self.frame_pub.publish(self.bag_msgs[self.frame_idx])
-        print("Published first frame")
+        # Radius slider
+        self.rad_slider = QSlider(Qt.Horizontal)
+        self.rad_slider.setRange(0, 100)
+        self.rad_slider.setValue(int(self.radius * 100))  # The radius passed in is a decimal, setval must be an int
+        self.rad_slider.valueChanged.connect(self.radius_change)
+        self.layout.addWidget(self.rad_slider)
+
+        # Frame Label
+        self.label = QLabel('0', self)
+        self.label.setAlignment(Qt.AlignCenter | Qt.AlignVCenter)
+        self.label.setText("Frame Selector")
+        self.layout.addSpacing(15)
+        self.layout.addWidget(self.label)
+
+        # Frame slider
+        self.frame_slider = QSlider(Qt.Horizontal)
+        self.frame_slider.setRange(0, len(self.bag_msgs) - 1)
+        self.frame_slider.setValue(0)
+        self.frame_slider.valueChanged.connect(self.frame_change)
+        self.layout.addWidget(self.frame_slider)
 
     def setup_sphere_marker(self):
+        # Initialize the parameters of the sphere which shows highlighted region
         self.sphere_marker.header.frame_id = self.frame_id
         self.sphere_marker.header.stamp = rospy.Time.now()
         self.sphere_marker.type = Marker.SPHERE
         self.sphere_marker.pose.position.x = 0
         self.sphere_marker.pose.position.y = 0
         self.sphere_marker.pose.position.z = 0
+        # The scales are 2 * radius because they are the diameters of the sphere
         self.sphere_marker.scale.x = self.radius * 2
         self.sphere_marker.scale.y = self.radius * 2
         self.sphere_marker.scale.z = self.radius * 2
@@ -124,97 +150,74 @@ class TrajectoryPlaybackGUI(QWidget):
         # 'commit' changes and send to all clients
         server.applyChanges()
 
-    def setup_UI(self):
-        # Radius Label
-        self.label = QLabel('0', self)
-        self.label.setAlignment(Qt.AlignCenter | Qt.AlignVCenter)
-        self.label.setText("Radius Selector")
-        self.layout.addSpacing(15)
-        self.layout.addWidget(self.label)
-
-        # Radius slider
-        self.rad_slider = QSlider(Qt.Horizontal)
-        self.rad_slider.setRange(0, 100)
-        self.rad_slider.setValue(int(self.radius * 100))  # The radius passed in is a decimal, setval must be an int
-        self.rad_slider.valueChanged.connect(self.radius_change)
-        self.layout.addWidget(self.rad_slider)
-
-        # Frame Label
-        self.label = QLabel('0', self)
-        self.label.setAlignment(Qt.AlignCenter | Qt.AlignVCenter)
-        self.label.setText("Frame Selector")
-        self.layout.addSpacing(15)
-        self.layout.addWidget(self.label)
-
-        # Frame slider
-        self.frame_slider = QSlider(Qt.Horizontal)
-        self.frame_slider.setRange(0, len(self.bag_msgs) - 1)
-        self.frame_slider.setValue(0)
-        self.frame_slider.valueChanged.connect(self.frame_change)
-        self.layout.addWidget(self.frame_slider)
-
-        self.frameNum = QLabel('0', self)
-        self.frameNum.setAlignment(Qt.AlignCenter | Qt.AlignVCenter)
-        self.frameNum.setMinimumWidth(80)
-
-        self.layout.addWidget(self.frameNum)
-
+    # Callback for moving the interactive marker
     def center_change(self, feedback):
+        # p is the x,y,z of the center of the marker
         p = feedback.pose.position
         self.center = np.array([p.x, p.y, p.z])
 
+        # Update visuals of the sphere marker
         self.sphere_marker.pose.position.x = self.center[0]
         self.sphere_marker.pose.position.y = self.center[1]
         self.sphere_marker.pose.position.z = self.center[2]
         self.sphere_pub.publish(self.sphere_marker)
 
+        # Select the new region
         self.select_pc()
 
     # Callback for slider change
     def radius_change(self):
+        # The values in the slider are always integer values, dividing by 100 scales it down to appropriate values
         self.radius = self.rad_slider.value() / 100.0
 
+        # Update visuals for radius of sphere
         self.sphere_marker.scale.x = self.radius * 2
         self.sphere_marker.scale.y = self.radius * 2
         self.sphere_marker.scale.z = self.radius * 2
         self.sphere_pub.publish(self.sphere_marker)
 
+        # Select the new region
         self.select_pc()
 
-    # Callback for slider change
+    # Callback for frame slider change
     def frame_change(self):
+        # Update the index of the frame we are on
         self.frame_idx = self.frame_slider.value()
-        self.frame_pub.publish(self.bag_msgs[self.frame_idx])
+
+        # Read from the bag the new pc
         self.points = np.array(list(sensor_msgs.point_cloud2.read_points(self.bag_msgs[self.frame_idx])))
-        self.frameNum.setText(str(self.frame_idx))
 
+        # Publish the entire pc
+        self.frame_pub.publish(self.bag_msgs[self.frame_idx])
+
+        # Select the new region
+        self.select_pc()
+
+    # Get the points in the point cloud which are within the radius of the center point
     def select_pc(self):
-        self.frame_counter += 1
-
-        if self.frame_counter % 2 != 0:
-            return
-
-        self.frame_counter = 0
-
         dist = np.linalg.norm(self.points[:, :3] - self.center, axis=-1)  # indices 0 to 3 is xyz, the 4th val is rgb
         sel_indices = np.argwhere(dist < self.radius).squeeze(1)
         sel_pc = self.points[sel_indices]
 
+        # Convert numpy array back to a point cloud message and publish
         msg = utils.points_to_pc2_msg(sel_pc, self.frame_id)
         self.sel_pub.publish(msg)
 
 
+# To run script: python pc_selector.py camera_depth_optical_frame 0.33
+# /home/christianforeman/catkin_ws/src/plant_time/plant_test4.bag
 def main():
     rospy.init_node("pc_selector")
 
     parser = argparse.ArgumentParser()
+    parser.add_argument('frame', type=str)
     parser.add_argument('radius', type=float)
     parser.add_argument('bag_filename', type=pathlib.Path)
 
     args = parser.parse_args(rospy.myargv(sys.argv[1:]))
 
     app = QApplication([])
-    gui = TrajectoryPlaybackGUI("camera_depth_optical_frame", args.radius, args.bag_filename)
+    gui = TrajectoryPlaybackGUI(args.frame, args.radius, args.bag_filename)
     gui.show()
     app.exec()
 
